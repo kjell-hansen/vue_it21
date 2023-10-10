a79a308b5401dede89d79c067db04058ee770ab3
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-10-10 10:51:08 +0300

    Utseende på appen klart

---
+++ ToDo/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+function addItem() {
+  this.item = ''
+}
+</script>
+
+<template>
+  <header>
+    <h1>Tasklist</h1>
+  </header>
+
+  <main>
+    <p>
+      Uppgift: <input type="text" v-bind="item" placeholder="Ange uppgift" />
+      <button v-on="addItem">Lägg till</button>
+    </p>
+    <h3 id="todo">Todo...</h3>
+    <h3 id="klart">Klart!</h3>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+h1 {
+  color: blue;
+}
+button {
+  margin-left: 0.5em;
+}
+#todo {
+  color: red;
+}
+#klart {
+  color: greenyellow;
+}
+</style>
