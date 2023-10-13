50feef72e41f5d4d13aa9f62cfe0b8558577b533
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-10-13 15:55:00 +0300

    Appen funkar, nu med komponenter

--- P2_StenSaxPåse/src/App.vue
+++ P2_StenSaxPåse/src/App.vue
@@ -1,30 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 import GameAlternatives from './components/GameAlternatives.vue'
+import GameScore from './components/GameScore.vue'
 
-const gameFinished = ref(false)
 const alternative = ref('')
 const compAlternative = ref('')
 const winnerInfo = ref('')
-const alternatives = ref(['Sten', 'Sax', 'Påse'])
 
-function alternativeChosen(e) {
-  if (gameFinished.value) {
-    return
-  }
-  alternative.value = e.target.innerText
-  e.target.classList.add('vald')
-  compAlternative.value = alternatives.value[Math.floor(Math.random() * alternatives.value.length)]
-  setWinnerInfo(alternative.value, compAlternative.value)
-  gameFinished.value = true
-}
-function resetGame() {
-  gameFinished.value = false
-  let buttons = document.getElementsByClassName('button')
-  for (let b of buttons) {
-    b.classList.remove('vald')
-  }
-}
 function setWinnerInfo(winner) {
   if (winner === 'draw') {
     winnerInfo.value = "It's a draw!"
@@ -49,48 +31,16 @@ function setComputerAlternative(alt) {
     @computer-choice="setComputerAlternative"
     @winner="setWinnerInfo"
   />
-  <ul>
-    <li v-for="alt in alternatives" @click="alternativeChosen" class="button">{{ alt }}</li>
-  </ul>
-  <p v-if="!gameFinished">Ready to play</p>
-  <div v-else id="result">
-    <p>Du valde {{ alternative }}</p>
-    <p>Jag valde {{ compAlternative }}</p>
-    <p>{{ winnerInfo }}</p>
-    <button @click="resetGame">Play again!</button>
-  </div>
+  <GameScore
+    :user-alternative="alternative"
+    :computer-alternative="compAlternative"
+    :winner-info="winnerInfo"
+  />
 </template>
 
 <style scoped>
 h1 {
   text-align: center;
   min-width: 10px;
-}
-ul {
-  display: flex;
-}
-li {
-  list-style-type: none;
-  margin: 0.5em;
-  background-color: lightblue;
-  padding: 1em;
-  border: 2px solid blue;
-  border-radius: 0.5em;
-}
-#result button {
-  margin: auto;
-  display: block;
-  min-width: 10px;
-  margin-top: 1em;
-}
-#result p {
-  margin: auto;
-  padding: 0.5em;
-  width: 50%;
-  background-color: aqua;
-}
-.vald {
-  background-color: pink;
-  border-color: red;
 }
 </style>
