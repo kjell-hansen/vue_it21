2160564492ca04d641a005d6e126a665135dacf1
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-10-27 15:59:58 +0300

    All data läggs in på olika kort

---
+++ P4_MittVader/src/components/WeatherCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps(['weatherData'])
+</script>
+
+<template>
+  <div id="card">
+    <p>
+      Datum: <span class="data">{{ props.weatherData.date }}</span>
+    </p>
+    <p>
+      Vädret väntas bli <span class="data">{{ props.weatherData.weatherCondition }}</span>
+    </p>
+    <p>
+      Temp: <span class="data">{{ props.weatherData.minTemp }}</span> -
+      <span class="data">{{ props.weatherData.maxTemp }}</span
+      >°C
+    </p>
+    <p>
+      Nederbörd: <span class="data">{{ props.weatherData.precipitation }}</span
+      >mm
+    </p>
+    <p>
+      Vind: <span class="data">{{ props.weatherData.windSpeed }}</span
+      >m/s från <span class="data">{{ props.weatherData.windDirection }}</span>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+#card {
+  background-color: aliceblue;
+  border: 1px solid navy;
+  min-height: 100px;
+  margin: 0.2em 1.5em;
+  padding: 0.3em;
+  border-radius: 1em;
+}
+.data {
+  font-weight: 200;
+  font-style: italic;
+  color: blue;
+}
+</style>
