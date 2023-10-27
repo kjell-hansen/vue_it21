a395a88d882a1f380bfd68f7178e44cf49c3d6cf
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-10-27 15:59:49 +0300

    All data läggs in på olika kort

--- P4_MittVader/src/views/HomeView.vue
+++ P4_MittVader/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import WeatherCard from '../components/WeatherCard.vue'
 
 const weatherAPI = ref(
   'https://api.open-meteo.com/v1/forecast?latitude=60.0973&longitude=19.9348&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant&windspeed_unit=ms&timezone=auto'
 )
-
+const weatherInfo = ref([])
 onMounted(() => {
   fetch(weatherAPI.value)
     .then((response) => {
@@ -16,12 +17,31 @@ onMounted(() => {
       if (data.length === 0) {
         throw 'Ingen data returnerades'
       }
-      console.log('Hurra!', data)
+      weatherInfo.value = []
+      let items = data.daily.time.length
+      for (let index = 0; index < items; index++) {
+        let wd = {}
+        wd.date = data.daily.time[index]
+        wd.weatherCondition = data.daily.weathercode[index]
+        wd.maxTemp = data.daily.temperature_2m_max[index]
+        wd.minTemp = data.daily.temperature_2m_min[index]
+        wd.precipitation = data.daily.precipitation_sum[index]
+        wd.windDirection = data.daily.winddirection_10m_dominant[index]
+        wd.windSpeed = data.daily.windspeed_10m_max[index]
+        weatherInfo.value.push(wd)
+      }
+      console.log('Hurra!!', weatherInfo)
     })
 })
 </script>
 <template>
-  <p>HomeView</p>
+  <div>
+    <WeatherCard v-for="day in weatherInfo" :key="day" :weather-data="day" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  overflow: auto;
+}
+</style>
