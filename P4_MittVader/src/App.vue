3fef90c82ab6e0a51308faa2a1befa8a270fe597
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-10-27 14:27:51 +0300

    Startup och första delen av appen

---
+++ P4_MittVader/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>Kjells väder</h1>
+  <p>När Kjell ger väder lyssnar man</p>
+  <RouterView />
+</template>
+
+<style scoped>
+h1,
+p {
+  text-align: center;
+}
+</style>
---
+++ P4_MittVader/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const weatherAPI = ref(
+  'https://api.open-meteo.com/v1/forecast?latitude=60.0973&longitude=19.9348&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant&windspeed_unit=ms&timezone=auto'
+)
+
+onMounted(() => {
+  fetch(weatherAPI.value)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json()
+      }
+    })
+    .then((data) => {
+      if (data.length === 0) {
+        throw 'Ingen data returnerades'
+      }
+      console.log('Hurra!', data)
+    })
+})
+</script>
+<template>
+  <p>HomeView</p>
+</template>
+
+<style scoped></style>
