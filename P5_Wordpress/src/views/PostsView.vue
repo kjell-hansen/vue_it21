c69174e60a883a2250c068629f47c0b7904fe448
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-11-15 10:25:01 +0200

    Lagt till vy för att visa alla poster

---
+++ P5_Wordpress/src/views/PostsView.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import router from '../router'
+import APIService from '../services/APIService'
+
+const authors = ref([])
+const categories = ref([])
+const tags = ref([])
+const posts = ref([])
+
+onMounted(() => {
+  let req = []
+  req.push(APIService.get('categories'))
+  req.push(APIService.get('users'))
+  req.push(APIService.get('tags?per_page=100'))
+  req.push(APIService.get('posts'))
+  Promise.all(req).then((responses) => {
+    categories.value = responses[0].map((itm) => {
+      return new Object({ id: itm.id, name: itm.name, slug: itm.slug })
+    })
+    authors.value = responses[1].map((itm) => {
+      return new Object({ id: itm.id, name: itm.name, slug: itm.slug })
+    })
+    tags.value = responses[2]
+    posts.value = responses[3]
+  })
+})
+</script>
+
+<template>
+  <h1>Posts</h1>
+  <main>
+    <div v-for="itm in posts" :key="itm">
+      <h2>
+        {{ itm.title.rendered }}
+      </h2>
+      <p class="date">{{ itm.modified?.substr(0, 10) }} {{ itm.modified?.substr(11) }}</p>
+      <p v-html="itm?.excerpt.rendered" @click="router.push('/Post/' + itm.slug)" class="excerpt" />
+      <p>
+        <span class="intense">Författare:</span>&nbsp;
+        <a
+          v-if="
+            authors.find((aut) => {
+              return aut.id === itm.author
+            })
+          "
+          :href="'/Author/' + authors.find((aut) => aut.id === itm.author).slug"
+          >{{
+            authors.find((aut) => {
+              return aut.id === itm.author
+            })?.name ?? itm.author
+          }}
+        </a>
+      </p>
+      <p>
+        <span class="intense">Kategorier:</span>&nbsp;
+        <span v-for="cat in itm.categories" :key="cat">
+          <a
+            v-if="categories.find((itm) => itm.id === cat)"
+            :href="categories.find((itm) => itm.id === cat)?.slug ?? ''"
+            >{{ categories.find((itm) => itm.id === cat)?.name ?? '' }}</a
+          >&nbsp;
+        </span>
+      </p>
+      <p>
+        <span class="intense">Tags:</span>&nbsp;
+        <span v-for="tag in itm.tags" :key="tag"
+          >{{ tags.find((itm) => itm.id === tag)?.name ?? tag }}
+          &nbsp;
+        </span>
+      </p>
+    </div>
+  </main>
+</template>
+<style scoped>
+.intense {
+  font-weight: bold;
+  text-decoration: underline;
+}
+.date {
+  text-align: right;
+  margin-right: 2em;
+  font-style: italic;
+}
+.excerpt {
+  cursor: pointer;
+}
+</style>
